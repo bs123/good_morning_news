246bfd3363ea1cabe05bf679719280a46e1d22eb
Hendrik Luehrsen <hendrik@luehrsen.de>
2016-02-19 23:23:47 +0100

    Update to the wordpress theme

--- www/wp-content/themes/goodmorning_theme/index.php
+++ www/wp-content/themes/goodmorning_theme/index.php
@@ -1,16 +1,5 @@
 <?php get_header(); ?>
 
-<div class="container">
-	<div class="row">
-	<?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
-
-    	<div <?php post_class("col-xs-12"); ?>>
-        	<h3><?php the_title(); ?></h3>
-        	<div><?php the_content(); ?></div>
-        </div>
-
-    <?php endwhile; endif; ?>
-	</div>
-</div>
+There is nothing here yet!
 
 <?php get_footer(); ?>
