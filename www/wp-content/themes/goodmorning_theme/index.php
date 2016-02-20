c86a3045747e39f5fa2eb54173b3804ac493df99
Hendrik Luehrsen <hendrik@luehrsen.de>
2016-02-20 20:44:42 +0100

    Beauty and shit

--- www/wp-content/themes/goodmorning_theme/index.php
+++ www/wp-content/themes/goodmorning_theme/index.php
@@ -1,24 +1,40 @@
 <?php
 	$user_info = get_userdata( get_current_user_id() );
 
+	if(is_user_logged_in()){
+		$name = $user_info->user_nicename;
+	} else {
+		$name = "Nachrichten";
+	}
+
 	get_header(); ?>
 
-<div class="prev-button" id="b_prev">
-	<a href="#prev" class="fa fa-angle-left"></a>
+<div class="loading-indicator"></div>
+<div class="page-wrapper">
+	<div class="prev-button" id="b_prev">
+		<a href="#prev" class="fa fa-angle-left"></a>
+	</div>
+	<ul id="master-list" class="cards">
+	    <li class="welcome-card card show">
+	    	<header style="background-image: url(<?php echo WP_THEME_URL; ?>/img/morgen.jpg)">
+		    	<h1 class="welcome-text">Guten Morgen, <br />
+		    	<span class="username"><?php echo $name; ?></span>
+		    	<span class="loading">Deine 10 Minuten Frühstücks-Nachrichten werden vorbereitet.</span></h1>
+	    	</header>
+	    </li>
+	    <li class="goodbye-card card">
+	    	<h2 class="goodbye-text">
+		    	Wir wünschen dir<br /> einen schönen Tag!
+	    	</h2>
+	    </li>
+	</ul>
+	<div class="next-button" id="b_next">
+		<a href="#next" class="fa fa-angle-right"></a>
+	</div>
 </div>
-<ul id="master-list" class="cards">
-    <li class="welcome-card card show">
-    	<h1 class="welcome-text">Guten Morgen, <br />
-    	<span class="username"><?php echo $user_info->user_nicename; ?></span></h1>
-    </li>
-    <li class="goodbye-card card">
-    	<h2 class="goodbye-text">
-	    	Wir wünschen dir einen schönen Tag!
-    	</h2>
-    </li>
-</ul>
-<div class="next-button" id="b_next">
-	<a href="#next" class="fa fa-angle-right"></a>
+<div class="footer">
+	<h3>Guten Morgen, Nachrichten!</h3>
+	<p>&copy; 2016 | von Flo, Jan & Hendrik für die #pulshackdays
 </div>
 
 <?php get_footer(); ?>
--- www/wp-content/themes/goodmorning_theme/js/main.js
+++ www/wp-content/themes/goodmorning_theme/js/main.js
@@ -1,123 +1,150 @@
 //@depend "plugins.js"
 //@dependx "constants.js"
 
+
+
 var postindex = -1;
+var loadingState = false;
 
-$(document).ready(function () {
-    //   $.get("http://goodmorning.devserv.de/wp-json/goodmorning-news/1.0/list-news", function (data, status) {
-    $.ajax({
-        url: WP_API_Settings.root + 'goodmorning-news/1.0/list-news/', method: 'GET', beforeSend: function (xhr) {
-            xhr.setRequestHeader('X-WP-Nonce', WP_API_Settings.nonce);
-        },
-    }).done(function (response) {
-        // console.log(response);
-        insert_posts(response);
-    });
+
+
+function setLoading(loading) {
+	loadingState = loading;
+	if (loading) {
+		$("body").addClass("loading");
+	} else {
+		$("body").removeClass("loading");
+	}
+}
+
+
+function isLoading() {
+	return loadingState;
+}
+
+$(document).ready(function() {
+
+	// Load the inital posts
+	setLoading(true);
+	$.ajax({
+		url: WP_API_Settings.root + 'goodmorning-news/1.0/list-news/',
+		method: 'GET',
+		beforeSend: function(xhr) {
+			xhr.setRequestHeader('X-WP-Nonce', WP_API_Settings.nonce);
+		},
+	}).done(function(response) {
+		// console.log(response);
+		insert_posts(response);
+		setLoading(false);
+	}).error(function(response) {
+		setLoading(false)
+	});
+
+
+	$("ul.cards").on("click", "li.card a.upvote", function(e) {
+		var $card = $(e.currentTarget).parents("li.card");
+		var $postId = $card.attr("postid");
+		setLoading(true);
+		$.ajax({
+			url: WP_API_Settings.root + 'goodmorning-news/1.0/upvote/' + $postId,
+			method: 'GET',
+			beforeSend: function(xhr) {
+				xhr.setRequestHeader('X-WP-Nonce', WP_API_Settings.nonce);
+			},
+		}).done(function(response) {
+			console.log("upvote successful");
+			$card.addClass("upvoted").removeClass("downvoted");
+			setLoading(false);
+		}).error(function(response) {
+			setLoading(false)
+		});
+	});
+
+
+	$("ul.cards").on("click", "li.card a.downvote", function(e) {
+		var $card = $(e.currentTarget).parents("li.card");
+		var $postId = $card.attr("postid");
+		setLoading(true);
+		$.ajax({
+			url: WP_API_Settings.root + 'goodmorning-news/1.0/downvote/' + $postId,
+			method: 'GET',
+			beforeSend: function(xhr) {
+				xhr.setRequestHeader('X-WP-Nonce', WP_API_Settings.nonce);
+			},
+		}).done(function(response) {
+			console.log("downvote successful");
+			$card.addClass("downvoted").removeClass("upvoted");
+			setLoading(false);
+		}).error(function(response) {
+			setLoading(false)
+		});
+	});
+
+
+	$("ul.cards").on("click", "li.card a.more_content", function(e) {
+		var $card = $(e.currentTarget).parents("li.card");
+		$card.toggleClass("content_open");
+	});
+
+
+	$("#b_next").click(function() {
+		if(isLoading()){
+			return;
+		}
+		var $current = $("ul.cards li.card.show");
+		var $next = $current.next("li.card");
+		if ($next.length > 0) {
+			$current.removeClass("show").removeClass("content_open");
+			$next.addClass("show");
+		}
+	});
+
+
+	$("#b_prev").click(function() {
+		if(isLoading()){
+			return;
+		}
+		var $current = $("ul.cards li.card.show");
+		var $next = $current.prev("li.card");
+		if ($next.length > 0) {
+			$current.removeClass("show").removeClass("content_open");
+			$next.addClass("show");
+		}
+	});
+
+
 });
-
-$(document).ready(function () {
-    $("#liveReload").click(function () {
-        $("#liveReload").fadeOut();
-    });
-});
-
-$(document).ready(function () {
-    $("ul.cards").on("click", "li.card a.upvote", function (e) {
-        var $card = $(e.currentTarget).parents("li.card");
-        var $postId = $card.attr("postid");
-        $.ajax({
-            url: WP_API_Settings.root + 'goodmorning-news/1.0/upvote/' + $postId,
-            method: 'GET',
-            beforeSend: function (xhr) {
-                xhr.setRequestHeader('X-WP-Nonce', WP_API_Settings.nonce);
-            },
-        }).done(function (response) {
-            $card.addClass("downvoted").removeClass("upvoted");
-        });
-    });
-
-    $("ul.cards").on("click", "li.card a.downvote", function (e) {
-        var $card = $(e.currentTarget).parents("li.card");
-        var $postId = $card.attr("postid");
-        $.ajax({
-            url: WP_API_Settings.root + 'goodmorning-news/1.0/downvote/' + $postId,
-            method: 'GET',
-            beforeSend: function (xhr) {
-                xhr.setRequestHeader('X-WP-Nonce', WP_API_Settings.nonce);
-            },
-        }).done(function (response) {
-            $card.addClass("downvoted").removeClass("upvoted");
-        });
-    });
-
-    $("ul.cards").on("click", "li.card a.more_content", function(e){
-	   var $card = $(e.currentTarget).parents("li.card");
-	   $card.toggleClass("content_open");
-    });
-});
-
-$(document).ready(function () {
-
-    $("#b_next").click(function(){
-	   var $current = $("ul.cards li.card.show");
-	   var $next = $current.next("li.card");
-
-	   if($next.length > 0){
-	   	$current.removeClass("show").removeClass("content_open");
-	   	$next.addClass("show");
-	   }
-    });
-
-     $("#b_prev").click(function(){
-	   var $current = $("ul.cards li.card.show");
-	   var $next = $current.prev("li.card");
-
-	   if($next.length > 0){
-	   	$current.removeClass("show").removeClass("content_open");
-	   	$next.addClass("show");
-	   }
-    });
-});
-
 
 function insert_posts(posts) {
-    //  console.log('+++++++++' + typeof (posts));
-    var duration = 0;
-    if (typeof (posts) == "object") {
-        for (var i in posts) {
-            var post = posts[i];
-            console.log(post);
-            duration = duration + post.consume_dur;
-            var $post = $("<li>").attr("id", "post-" + i).addClass("article card").attr("postid", post.id);
-
-            // Construct the post header
-            $subheadline = $("<span>").addClass("small").html(post.headline);
-            $headline = $("<h1>").html(post.title).prepend($subheadline);
-            $header = $("<header>").addClass("article-header").append($headline).css("background-image", "url("+post.thumbnail+")");
+	//  console.log('+++++++++' + typeof (posts));
+	var duration = 0;
+	if (typeof(posts) == "object") {
+		for (var i in posts) {
+			var post = posts[i];
+			console.log(post);
+			duration = duration + post.consume_dur;
+			var $post = $("<li>").attr("id", "post-" + i).addClass("article card").attr("postid", post.id);
+			// Construct the post header
+			$subheadline = $("<span>").addClass("small").html(post.headline);
+			$headline = $("<h1>").html(post.title).prepend($subheadline);
+			$header = $("<header>").addClass("article-header").append($headline).css("background-image", "url(" + post.thumbnail + ")");
 			$post.append($header);
-
 			// Construct the button bar
 			$downvote_link = $("<a>").attr("href", "#downvote").addClass("downvote fa fa-thumbs-o-down").attr("id", "b_rate_down");
 			$upvote_link = $("<a>").attr("href", "#upvote").addClass("upvote fa fa-thumbs-o-up").attr("id", "b_rate_up");
 			$more_link = $("<a>").attr("href", "#upvote").addClass("more_content fa fa-angle-down");
 			$buttons = $("<div>").addClass("buttons").append($downvote_link).append($more_link).append($upvote_link);
-
 			$post.append($buttons);
-
 			// Construct the Content
 			var $datetime = $("<div>").addClass("datetime").html(post.date);
 			var $content = $("<div>").addClass("content clearfix").html(post.content).prepend($datetime);
-
-            $post.append($content);
-
-            $("#master-list").append($post);
-            if (duration >= 1000)
-                break;
-        }
-        console.log("duration : " + duration);
-    }
-
-    var $goodbyeCard = $("li.card.goodbye-card");
-    $goodbyeCard.remove();
-    $("ul.cards").append($goodbyeCard);
+			$post.append($content);
+			$("#master-list").append($post);
+			if (duration >= 1000) break;
+		}
+		console.log("duration : " + duration);
+	}
+	var $goodbyeCard = $("li.card.goodbye-card");
+	$goodbyeCard.remove();
+	$("ul.cards").append($goodbyeCard);
 }
