f18a9d2806bb3c67594389f8f4c6691c0c665a0a
bs123 <dojo@nospam.tk>
2016-02-20 16:52:49 +0100

    added show single article

--- www/wp-content/themes/goodmorning_theme/js/main.js
+++ www/wp-content/themes/goodmorning_theme/js/main.js
@@ -8,8 +8,18 @@ $(document).ready(function () {
             xhr.setRequestHeader('X-WP-Nonce', WP_API_Settings.nonce);
         },
     }).done(function (response) {
-        console.log(response);
+       // console.log(response);
         insert_posts(response);
+
+        $(".article").click(function () {
+            var $this = $(this);
+            console.log($this);
+            // var $article = $this.parents("li.article");
+            var $content = $(".content", $this);
+            //   $content.removeClass("panel").addClass("headline").slideDown(constants.SLIDE_DOWN);
+            console.log($content);
+            $content.removeClass("hidden").addClass("shown");
+        });
     });
 
 });
@@ -24,27 +34,23 @@ $(document).ready(function () {
 });
 
 $(document).ready(function () {
-    $(".article").click(function () {
-        var $this = $(this);
-        // var $article = $this.parents("li.article");
-        var $content = $(".content", $this);
-        //   $content.removeClass("panel").addClass("headline").slideDown(constants.SLIDE_DOWN);
-        console.log($content)
-        $content.removeClass("hidden").addClass("shown");
-    });
 });
 
-
 $(document).ready(function () {
+    var postindex = 0;
     $("#b_next").click(function () {
-        $("#post-1").removeClass("hidden").addClass("shown");
+        $("#post-" + (postindex-1)).addClass("hidden").removeClass("shown");
+        $("#post-" + postindex).removeClass("hidden").addClass("shown");
         // $("#post-1").toggle();
+        postindex ++;
+        console.log($(".article"));
+
     });
 });
 
 
 function insert_posts(posts) {
-    console.log('+++++++++' + typeof (posts));
+  //  console.log('+++++++++' + typeof (posts));
     var duration = 0;
     if (typeof (posts) == "object") {
         for (var i in posts) {
