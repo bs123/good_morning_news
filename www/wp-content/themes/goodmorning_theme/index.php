cad7810b1e874a785396000a99b515464c61c0a7
Hendrik Luehrsen <hendrik@luehrsen.de>
2016-02-20 16:56:37 +0100

    first rudimentary styles

--- www/wp-content/themes/goodmorning_theme/index.php
+++ www/wp-content/themes/goodmorning_theme/index.php
@@ -1,17 +1,28 @@
-<?php get_header(); ?>
+<?php
+	$user_info = get_userdata( get_current_user_id() );
 
-<p id="liveReload"> hi live reload, bist du da? </p>
+	get_header(); ?>
 
 <ul id="master-list" class="cards">
-    <li class="welcome-card showen">  </li>
-    <li class="goodbye-card hidden">  </li>
+    <li class="welcome-card card show">
+    	<h1 class="welcome-text">Guten Morgen, <br />
+    	<span class="username"><?php echo $user_info->user_nicename; ?></span></h1>
+    </li>
+    <li class="goodbye-card card">
+    	<h2 class="goodbye-text">
+	    	Wir wünschen dir einen schönen Tag!
+    	</h2>
+    </li>
 </ul>
+
+<div class="buttons">
 
 <button id="b_next">next</button>
 <button id="b_last">last</button>
 
 <button id="b_rate_down">rate_down</button>
 <button id="b_rate_up">rate_up</button>
+</div>
 
 
 <?php get_footer(); ?>
