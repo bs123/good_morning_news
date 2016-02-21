2b95a60e6e1dadf31b1bd52535296a801e7fe744
Hendrik Luehrsen <hendrik@luehrsen.de>
2016-02-21 09:27:18 +0100

    Login Redirect

--- www/wp-content/themes/goodmorning_theme/functions.php
+++ www/wp-content/themes/goodmorning_theme/functions.php
@@ -66,3 +66,29 @@ function lh_load_theme_textdomain(){
     load_theme_textdomain(LANG_NAMESPACE, get_template_directory() . '/lang');
 }
 add_action('after_setup_theme', 'lh_load_theme_textdomain');
+
+/**
+ * Redirect user after successful login.
+ *
+ * @param string $redirect_to URL to redirect to.
+ * @param string $request URL the user is coming from.
+ * @param object $user Logged user's data.
+ * @return string
+ */
+function my_login_redirect( $redirect_to, $request, $user ) {
+	//is there a user to check?
+	global $user;
+	if ( isset( $user->roles ) && is_array( $user->roles ) ) {
+		//check for admins
+		if ( in_array( 'administrator', $user->roles ) ) {
+			// redirect them to the default place
+			return $redirect_to;
+		} else {
+			return home_url();
+		}
+	} else {
+		return $redirect_to;
+	}
+}
+
+add_filter( 'login_redirect', 'my_login_redirect', 10, 3 );
