ccccbab7515b8ee5ed6c8c650b0f7e81a707c1c6
Hendrik Luehrsen <hendrik@luehrsen.de>
2016-02-21 09:29:53 +0100

    Hide the Admin Bar

--- www/wp-content/themes/goodmorning_theme/functions.php
+++ www/wp-content/themes/goodmorning_theme/functions.php
@@ -90,5 +90,12 @@ function my_login_redirect( $redirect_to, $request, $user ) {
 		return $redirect_to;
 	}
 }
-
 add_filter( 'login_redirect', 'my_login_redirect', 10, 3 );
+
+add_action('after_setup_theme', 'remove_admin_bar');
+
+function remove_admin_bar() {
+	if (!current_user_can('administrator') && !is_admin()) {
+	  show_admin_bar(false);
+	}
+}
